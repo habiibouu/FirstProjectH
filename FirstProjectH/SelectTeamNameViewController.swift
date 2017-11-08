b030f001818fd6d88e2eeeeb2738f867c190dfc8
Kevin Bravo <bravo.kevin.contact@gmail.com>
2017-11-08 16:02:50 +0100

    added few changes

---
+++ FirstProjectH/SelectTeamNameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FirstProjectH
+//
+//  Created by Kevin Bravo on 06/11/2017.
+//  Copyright © 2017 Kevin Bravo. All rights reserved.
+//
+
+import UIKit
+
+class SelectTeamNameViewController: UIViewController {
+
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        instructionLabel.text = "Bonjour comment ca va"
+    }
+    
+    
+    
+}
+
