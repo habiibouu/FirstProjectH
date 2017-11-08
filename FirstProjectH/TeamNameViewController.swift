be3b318bffaac939eafe54c0dc3c7b122a07594f
Kevin Bravo <bravo.kevin.contact@gmail.com>
2017-11-08 16:20:34 +0100

    added textfield behavior

---
+++ FirstProjectH/TeamNameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TeamNameViewController.swift
+//  FirstProjectH
+//
+//  Created by Kevin Bravo on 08/11/2017.
+//  Copyright © 2017 Kevin Bravo. All rights reserved.
+//
+
+import UIKit
+
+class TeamNameViewController: UIViewController {
+
+    @IBOutlet weak var teamSelectionTitle: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBAction func debugButtonPressed() {
+        print(nameTextField.text)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        teamSelectionTitle.text = "Hello, merci de selectionner un nom d'equipe"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
